976b4c5739df5e6556114721af729425f7e61f92
Muzani S Pamasarsa <smuzani29@gmail.com>
2021-09-22 11:00:26 +0700

    add controller

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,25 @@ const { User } = require('../models')
 
 class UserController{
   
+  static async register(req, res){
+    try {
+      const payload = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        gender: req.body.gender,
+        state: req.body.state
+      }
+
+      
+    } catch (error) {
+      
+    }
+  }
+
+  static async login (req, res){
   
+  }
 }
 
 module.exports = UserController
