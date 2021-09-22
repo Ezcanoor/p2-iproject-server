e930d8fed83997e19d74935a960107822d13aca9
Muzani S Pamasarsa <smuzani29@gmail.com>
2021-09-22 20:13:57 +0700

    made a setup for chat, add controller and router

--- router/index.js
+++ router/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const { authentication } = require('../middleware/authentication')
 const router = express.Router()
 const userRouter = require('./userRouter')
+const charRouter = require('./chatRouter')
 
 router.use(userRouter)
 router.use(authentication)
-
+router.use(charRouter)
 module.exports = router
